c845bc68a5965c7bb34aae10bbea80fa0b2243ac
Ace Chua <acechua@Aces-MacBook-Pro.local>
2018-05-06 14:43:39 +0800

    created basic logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log("it works");
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	console.log('true');
+}
+if (cardsInPlay[0] == cardsInPlay[1]) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
